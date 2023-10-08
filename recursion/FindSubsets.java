5ec7996d1d72483c5455febd1f4c8003ce4578e1
Moin Malik <themoinmalik@gmail.com>
2023-10-08 09:59:11 +0530

    new changes

--- recursion/FindSubsets.java
+++ recursion/FindSubsets.java
@@ -1,5 +1,6 @@
 package src.recursion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FindSubsets {
@@ -7,11 +8,33 @@ public class FindSubsets {
 
     public static void main(String[] args) {
 
-        // 123;
+        List<Integer> list = List.of(1,2,3);
+        List<List<Integer>> ans = subsets(list);
+        System.out.println(ans);
 
     }
 
-    public static void subsets(int[] arr){
+    public static void calSubsets(List<Integer> arr, List<List<Integer>> res, List<Integer> subset, int ind){
+
+        res.add(new ArrayList<>(subset));
+
+        for (int i = ind; i < arr.size(); i++) {
+            subset.add(arr.get(i));
+            calSubsets(arr,res,subset,i+1);
+            subset.remove(subset.size()-1);
+        }
+    }
+
+    public static List<List<Integer>> subsets(List<Integer> arr){
+
+        List<Integer> subset = new ArrayList<>();
+        List<List<Integer>> res =  new ArrayList<>();
+
+        int index = 0;
+
+        calSubsets(arr, res,subset,index);
+
+        return res;
 
     }
 
