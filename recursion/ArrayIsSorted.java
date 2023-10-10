ec051bd4263e331dcc965d7d3ad7d3b653a72a2b
Moin Malik <themoinmalik@gmail.com>
2023-10-10 09:46:58 +0530

    new changes

---
+++ recursion/ArrayIsSorted.java
@@ -0,0 +1,46 @@
+package src.recursion;
+
+public class ArrayIsSorted {
+
+
+    public static void main(String[] args) {
+
+
+        int[] arr = new int[]{1,2,3,4,5};
+        int n = arr.length;
+        boolean ans = sortedRecur(arr,n);
+        System.out.println(ans);
+
+    }
+
+
+    public static boolean sorted(int[] arr){
+
+        for (int i = 0; i < arr.length-1; i++) {
+
+            if (arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+
+    /// recursive call...
+    public static boolean sortedRecur(int[] arr, int i){
+
+        i = i-1;
+        if (i == 1){
+            return true;
+        }
+
+        if (arr[i]>arr[i-1]){
+            return sortedRecur(arr,i-1);
+        }
+
+        return false;
+
+    }
+
+}
