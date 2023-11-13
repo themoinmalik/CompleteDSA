88e8d767488280f1ef3311c78e3d9e396ab1b2a0
Moin Malik <themoinmalik@gmail.com>
2023-11-13 16:28:25 +0530

    new changes

--- recursion_backtracking/PowerOfNum.java
+++ recursion_backtracking/PowerOfNum.java
@@ -6,19 +6,39 @@ public class PowerOfNum {
 
         int a = 2;
         int b = 3;
-        int ans = power(a,b);
+        int ans = power(a, b);
         System.out.println(ans);
+
+        int n = 16;
+        boolean ans1 = isPower(n);
+        System.out.println(ans1);
 
     }
 
-    public static int power(int a, int b){
+    public static int power(int a, int b) {
 
-        if (b==1){
+        if (b == 1) {
             return a;
         }
 
-        return power(a,b-1)*a;
+        return power(a, b - 1) * a;
 
 
     }
+
+    // power of 2, 3,4... yes or no...
+
+    public static boolean isPower(int n){
+
+        if (n==1){
+            return true;
+        }
+
+        if (n<=1  || n%2!=0){
+            return false;
+        }
+        return isPower(n/2);
+
+    }
+
 }
