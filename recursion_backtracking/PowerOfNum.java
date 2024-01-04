c21d6896ca43463f386440721102f31b5b666856
Moin Malik <themoinmalik@gmail.com>
2024-01-04 10:31:08 +0530

    new chnages

--- recursion_backtracking/PowerOfNum.java
+++ recursion_backtracking/PowerOfNum.java
@@ -13,6 +13,11 @@ public class PowerOfNum {
         boolean ans1 = isPower(n);
         System.out.println(ans1);
 
+        int[] arr = new int[]{1,2,4,5};
+        int e = 4;
+        int ans2 = searchArr(arr,e);
+        System.out.println(ans2);
+
     }
 
     public static int power(int a, int b) {
@@ -27,7 +32,6 @@ public class PowerOfNum {
     }
 
     // power of 2, 3,4... yes or no...
-
     public static boolean isPower(int n){
 
         if (n==1){
@@ -40,5 +44,20 @@ public class PowerOfNum {
         return isPower(n/2);
 
     }
+
+
+    // this is hwo basic search work in an array ...
+    public static int searchArr(int[] arr, int e){
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (e == arr[i]){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    // reverse an
 
 }
