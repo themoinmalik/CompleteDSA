90ab4c3b7093c510c9c9eb91cb4aeff04cf5d90b
Moin Malik <themoinmalik@gmail.com>
2024-02-28 10:51:23 +0530

    combination sum question added

--- recursion_backtracking/Combination.java
+++ recursion_backtracking/Combination.java
@@ -9,6 +9,7 @@ class Combination {
         List<List<Integer>> result = new ArrayList<>();
         combinationSum(candidates, target, result, new ArrayList<>(), 0, 0);
         return result;
+
     }
 
     private void combinationSum(int[] candidates, int target, List<List<Integer>> result, List<Integer> path, int sum, int start) {
@@ -20,19 +21,20 @@ class Combination {
 
         if (sum == target) { // success base case
             result.add(new ArrayList<>(path));
-            return;
         }
 
         // recursive case
         for (int i = start; i < candidates.length; i++) {
 
+            if (i>start && candidates[i] == candidates[i-1]){ // skipped, if same number found consecutively
+                continue;
+            }
+
             sum += candidates[i];
             path.add(candidates[i]);
-
-            combinationSum(candidates, target, result, path, sum, i);
-
+            combinationSum(candidates, target, result, path, sum, i+1);
             // backtracking
-            sum -= candidates[i];
+//            sum -= candidates[i];
             path.remove(path.size() - 1);
         }
 
