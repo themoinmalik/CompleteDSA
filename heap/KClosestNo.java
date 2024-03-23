32022bae23c130e5deda76da98068cb3818e2d0f
Moin Malik <moin.malik@tothenew.com>
2024-03-23 21:58:29 +0530

    more heap questions

---
+++ heap/KClosestNo.java
@@ -0,0 +1,19 @@
+package src.heap;
+
+import java.security.KeyPair;
+import java.util.Comparator;
+import java.util.*;
+
+
+public class KClosestNo {
+
+
+    public static void kClosestNo(int[] arr, int k){
+
+        PriorityQueue<Pair<Integer, Integer> > pq =
+                new PriorityQueue<>((a, b) -> b.getKey() - a.getKey());
+
+
+
+    }
+}
