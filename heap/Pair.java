474a9cc5f5fb92510ff2aa5351fe7eec202b3f0c
Moin Malik <moin.malik@tothenew.com>
2024-03-25 22:35:08 +0530

    K closet qwuestion in heap added

---
+++ heap/Pair.java
@@ -0,0 +1,13 @@
+package src.heap;
+
+public class Pair {
+
+    int index;
+    int element;
+
+
+    public Pair(int index, int element) {
+        this.index = index;
+        this.element = element;
+    }
+}
