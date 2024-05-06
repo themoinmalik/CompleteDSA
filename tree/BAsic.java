8c01831b980fb0a3cbccdf36261e7f10ef999824
Moin Malik <moin.malik@tothenew.com>
2024-05-06 09:39:44 +0530

    stating tree dsa

--- tree/BAsic.java
+++ tree/BAsic.java
@@ -13,7 +13,33 @@ public class BAsic {
 
     public int heigth(TreeNode root){
 
-        return root.left;
+        return 0;
+
+    }
+
+
+    /// check symmetric...
+
+    public boolean isSymmetric(TreeNode root){
+
+        return check(root, root);
+
+
+    }
+
+    public boolean check(TreeNode left, TreeNode right){
+
+        if (left == null && right == null){
+            return true;
+        }
+        if (left == null || right == null){
+            return false;
+        }
+        if (left.val != right.val){
+            return false;
+        }
+
+        return check(left.left, right.right) && check(left.right, right.left);
 
     }
 }
