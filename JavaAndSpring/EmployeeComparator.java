aad1a7930ba8104cfbe22ffa9f9698f17b7c4420
moin.malik <moin.malik@tothenew.com>
2024-09-25 15:00:03 +0530

    adding new questions

--- JavaAndSpring/EmployeeComparator.java
+++ JavaAndSpring/EmployeeComparator.java
@@ -1,13 +1,20 @@
 package src.JavaAndSpring;
 
 
+import src.heap.Pair;
+
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class EmployeeComparator implements Comparator<EmployeeComparable> {
 
 
     public int compare(EmployeeComparable o1, EmployeeComparable o2){
+        PriorityQueue<Pair<Integer, Integer>> min = new PriorityQueue<>();
         return o1.getName().compareTo(o2.getName());
     }
 
 }
+
+
+
