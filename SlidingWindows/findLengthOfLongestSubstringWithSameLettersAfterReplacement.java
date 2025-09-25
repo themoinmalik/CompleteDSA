4ce233f8f3855c7615e1bdfeeb27249ba488c45a
Moin Malik <moimalik@deloitte.com>
2025-09-25 13:39:49 +0530

    fixing code

--- SlidingWindows/findLengthOfLongestSubstringWithSameLettersAfterReplacement.java
+++ SlidingWindows/findLengthOfLongestSubstringWithSameLettersAfterReplacement.java
@@ -17,35 +17,25 @@ public class findLengthOfLongestSubstringWithSameLettersAfterReplacement {
 
     private static int checkLongestSubString(String str, int k) {
 
-
-        Integer i =0;
+        int i =0;
         Map<Character, Integer> map = new HashMap<>();
         int ans = Integer.MIN_VALUE;
 
-        for (Integer j = 0; j < str.length(); j++) {
+        for (int j = 0; j < str.length(); j++) {
             char c = str.charAt(j);
             map.put(c, map.getOrDefault(c, 0)+1);
-
-            // find tyhe max coutn character...
+            // find the max count character...
             Optional<Integer> maxcount = map.values().stream().max(Integer::compareTo);
-
             while (j-i+1 - maxcount.get() >k){
-
                 char ch = str.charAt(i);
                 map.put(ch, map.get(ch)-1);
                 i++;
             }
-
             if (j-i+1 - maxcount.get() == k){
                 ans = Math.max(ans, j-i+1);
             }
-
         }
-
-
        return ans;
-
-
     }
 
 
